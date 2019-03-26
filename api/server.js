1688ae1a48be69b9c3b7376f896f168692d164d8
Joshua Wood <JerichoWall@apu.edu>
2019-03-26 13:54:11 -0700

    Finished stretch

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const zooRouter = require('../zoos/zoos-router.js');
+const bearRouter = require('../bears/bears-router.js');
 
 
 const server = express();
@@ -9,5 +10,6 @@ server.use(helmet());
 server.use(express.json());
 
 server.use('/api/zoos', zooRouter);
+server.use('/api/bears', bearRouter)
 
 module.exports = server;
